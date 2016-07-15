6ea664c7538a6e295ea5c69684703a43a219335d
jbbraley <jbbraley@gmail.com>
2016-07-15 13:17:39 -0400

    worked on some instrumentation plan slides, and some sensitivity studies.

--- Analysis&Results/Prelim Analysis/DOF Sensitivity/PlotDOFModes.m
+++ Analysis&Results/Prelim Analysis/DOF Sensitivity/PlotDOFModes.m
@@ -6,12 +6,15 @@ cd(pname)
 addpath(genpath('C:\Users\John\Projects_Git\ramps'));
 addpath(genpath('C:\Users\John\Projects_Git\vma'));
 NID_fname = 'NodeID1.csv';
+BNID_fname = 'BoundNodes.csv';
 model_name = '2-span_APriori_wPiers';
 result_name = '2-span_APriori_wPiers';
 nmodes = 10;
 
+% Load nodes of interest
 NodeID = dlmread([pname NID_fname], '\n');
-
+% Load Boundary Nodes
+BNodes = dlmread([pname BNID_fname], '\n');
 %% Initialize St7 API library
 InitializeSt7();
 
@@ -21,6 +24,7 @@ St7OpenModelFile(uID, pname, model_name, ScratchPath);
 
 %% Get undeformed node coordinates
 Coords = getNodeLoc(uID, NodeID');
+Bcoords =  getNodeLoc(uID, BNodes');
     
 %% Pull Node Results
 FilePath = [pname result_name];
@@ -30,6 +34,8 @@ NodeRes = Get_NFARes(uID, FilePath, NodeID', nmodes);
 St7CloseModelFile(uID)
 
 %%Plot underformed shape
+ah = figure;
+modeInterpSparse(Coords(:,1), Coords(:,2), NodeRes(:,mode,3), Bcoords(:,1), Bcoords(:,2), zeros(size(Bcoords,1),1), 100);
 scale = 20000;
 mode = 10;
 figure
