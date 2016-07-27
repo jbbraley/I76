c5db90f30b59c044812e8212501099a38156db57
John DeVitis <johndevitis@gmail.com>
2016-07-27 03:05:08 -0400

    updated ambient processing code

--- code/I76_Ambient_Processing.m
+++ code/I76_Ambient_Processing.m
@@ -3,10 +3,16 @@
 % John DeVitis
 % July 28th, 2016
 %
+%
 % vma fcns called:
-%   getrms
-%   getpsd
-%   getcpsd
+%
+% * getrms
+% * getpsd
+% * getcpsd
+% * plot_interpmode
+% * searchVector
+% * normalizeMode
+
 
 %% Superstructure Instrumentation Plan
 %
@@ -15,6 +21,7 @@
 % 
 % <<instrumentation_superstructure.png>>
 %
+
 
 %% Substructure Instrumentation
 %
@@ -64,17 +71,15 @@ t = 0:dt:length(data)*dt-dt;    % form time vector [seconds]
 tMins = t./60;                  % tim vector [minutes]
 
 
-%% DOF Indices & Labels
+%% DOF Indicies & Labels
 %
 % Indices for vertical, transverse, and longitudinal DOF with corresponding
-% DOF coordinates
+% DOF labels
 %
 % *Notes:*
 %
-% * Indices are the respective column index for data array
+% * Indices are the respective column index for referencing the data matrix
 % 
-% * Strand7 model (0,0) starts at first right *interior* girder. These
-% coordinates have the (0,0) starting at the first right *exterior* girder.
 
 % vertical dof indices
 dof.vert.super = 1:18;
@@ -90,7 +95,7 @@ dof.long.sub = [25 26];
 dof.long.all = [dof.long.super dof.long.sub];
 
 % index for all dof
-dof.all = [dof.vert.all dof.trans.sub dof.long.all];
+dof.all = 1:size(data,2);
 
 % build dof legend
 leg = cell(1,length(dof.all));
@@ -99,11 +104,93 @@ for ii = 1:length(leg)
 end
 dof.labels = leg; % assign to dof struct
 
+
+%% DOF Coordinates
+%
+% *Notes:*
 % 
-% % grab general dimensions from model
-% oneSpanLength = 136.5;  % ft
-% oneSpanWidth = 67.3;    % ft
-% oneGirderLine = [.25 .5 .75 1.25] * oneSpan;
+% * x - longitudinal, y - transverse, z - vertical
+% * z dimension zero point -> centerline of girder
+% * dimensions in feet
+% * Strand7 model (0,0) starts at first right *interior* girder. These
+% coordinates have the (0,0) starting at the first right *exterior* girder.
+
+% grab general dimensions from model
+oneSpanLength = 136.5;  
+oneSpanWidth = 67.3;   
+oneGirderLine = [.25 .5 .75 ]*oneSpanLength;
+girderSpacing = 10; 
+numberOfGirdersPerSpan = 4;
+girderCenterLines(1:4) = (0:numberOfGirdersPerSpan-1)*girderSpacing;
+spanTransSpacing = 8; % distance between span 1 and span 2 exterior girders
+girderCenterLines(5:8) = girderCenterLines(4) + 8 + girderCenterLines(1:4);
+
+% pre-allocate coordinate array
+coords = zeros(size(data,2),3);
+
+% vertical dof - x dimension
+coords(1:3,1) = oneGirderLine;
+coords(4,1) = oneGirderLine(2);
+coords(5:7,1) = oneGirderLine;
+coords(8:10,1) = oneGirderLine;
+coords(11,1) = oneGirderLine(2);
+coords(12:14,1) = oneGirderLine;
+coords(15:18,1) = oneSpanLength * 1.25; % dof on second span quarter point
+
+% vertical dof - y dimension
+% girder 8 dof
+coords([1:3 15],2) = girderCenterLines(8);
+% dof 4 - halfway inbetween girders 6 and 7
+coords(4,2) = girderSpacing/2 + girderCenterLines(6); 
+% girder 5 dof
+coords([5:7 16],2) = girderCenterLines(5);
+% girder 4 dof
+coords([8:10 17],2) = girderCenterLines(4);
+% dof 11 - halfway inbetween girders 2 and 3
+coords(11,2) = girderSpacing/2 + girderCenterLines(2);
+% girder 1 dof
+coords([12:14 18],2) = girderCenterLines(1);
+
+% vertical dof - z dimension - leave at zero (from pre-allocation)
+
+
+% boundary coordinates - x dim
+leftBoundary = 1:8;
+midBoundary = 9:16;
+rightBoundary = 17:24;
+
+bcoords = zeros(size(data,2),3);
+bcoords(leftBoundary,1) = 0;
+bcoords(midBoundary,1) = oneSpanLength;
+bcoords(rightBoundary,1) = oneSpanLength*2;
+
+% boundary coords - y dim
+bcoords(leftBoundary,2) = girderCenterLines;
+bcoords(midBoundary,2) = girderCenterLines;
+bcoords(rightBoundary,2) = girderCenterLines;
+
+
+% create figure w/ increased resolution (from default)
+figWidth = 1120; % pixels
+figHeight = 840;
+rect = [0 50 figWidth figHeight];
+figure('OuterPosition', rect)
+
+
+% plot vertical dof and boundary nodes to check spatial dof
+scatter(coords(:,1),coords(:,2),'o','r','filled');
+hold all
+scatter(bcoords(:,1),bcoords(:,2),'o','k','filled')
+axis equal
+hold off
+title('Experimental DOF and Boundary Nodes')
+xlabel('Longitudinal Dimension - [ft]')
+ylabel('Transverse Dimension - [ft]');
+
+% assign to dof structure
+dof.coords = coords;
+dof.bcoords = bcoords;
+
 
 
 %% Data Stats
@@ -124,10 +211,31 @@ disp(rmsData);
 
 % get rms stats & display
 [Y,I] = min(rmsRaw);
-fprintf('\n\nMinimum RMS - DOF %s - Value %s[g]\n',num2str(I),num2str(Y));
+fprintf('\n\nMinimum RMS - ALL - DOF %s - Value %s g\n',num2str(I),num2str(Y));
 
 [Y,I] = max(rmsRaw);
-fprintf('Minimum RMS - DOF %s - Value %s[g]\n',num2str(I),num2str(Y));
+fprintf('Minimum RMS - ALL - DOF %s - Value %s g\n',num2str(I),num2str(Y));
+
+% get max/min for vertical dof
+ind = dof.vert.all;
+[Y,I] = min(rmsRaw(ind));
+fprintf('\n\nMinimum RMS - Vertical - DOF %s - Value %s g\n',num2str(ind(I)),num2str(Y));
+[Y,I] = max(rmsRaw(ind));
+fprintf('Maximum RMS - Vertical - DOF %s - Value %s[g]\n',num2str(ind(I)),num2str(Y));
+
+% get max/min for transverse dof
+ind = dof.trans.sub;
+[Y,I] = min(rmsRaw(ind));
+fprintf('\n\nMinimum RMS - Transverse - DOF %s - Value %s[g]\n',num2str(ind(I)),num2str(Y));
+[Y,I] = max(rmsRaw(ind));
+fprintf('Maximum RMS - Transverse - DOF %s - Value %s[g]\n',num2str(ind(I)),num2str(Y));
+
+% get max/min for vertical dof
+ind = dof.long.all;
+[Y,I] = min(rmsRaw(ind));
+fprintf('\n\nMinimum RMS - Longitudinal - DOF %s - Value %s[g]\n',num2str(ind(I)),num2str(Y));
+[Y,I] = max(rmsRaw(ind));
+fprintf('Maximum RMS - Longitudinal - DOF %s - Value %s[g]\n',num2str(ind(I)),num2str(Y));
 
 
 %% Time Plots - Vertical DOF
@@ -139,7 +247,6 @@ figWidth = 1120; % pixels
 figHeight = 840;
 rect = [0 50 figWidth figHeight];
 figure('OuterPosition', rect)
-
 
 % plot vertical superstructure
 ind = dof.vert.super;
@@ -168,6 +275,7 @@ ah = gca;
 ah.FontName = 'Times';
 ah.FontSize = 18;
 snapnow
+
 
 %% Time Plots - Transverse DOF 
 %
@@ -370,6 +478,125 @@ ah = gca;
 ah.FontName = 'Times';
 ah.FontSize = 18;
 snapnow
+
+
+
+%% Cross-Power Spectral Density (CPSD) - Vertical - Superstructure ONLY
+%
+% Get CPSD for phase information and mode shape plotting
+%
+% *Notes:*
+%
+% * Reference: DOF 18 
+% * Number of Averages: Data record broken into 50 segments for averaging
+% * Percent Overlap: 75 percent overlap of each segment
+% * NFFT: Default nfft
+%
+
+refInd = 14; % reference dof index
+nAvg = 50;  % number of averages
+perc = 75;  % percent overlap
+nfft = [];  % use default nfft lines
+
+% get cpsd
+[pxy,f] = getcpsd(data,refInd,nAvg,perc,nfft,fs);
+
+% display frequency resolution
+fprintf('\n\nObservable frequency range: 0-%s[Hz]\n',num2str(f(end)));
+fprintf('Frequency resolution: %s[Hz]\n',num2str(f(2)));
+
+
+% create figure w/ increased resolution (from default)
+figWidth = 1120; % pixels
+figHeight = 840;
+rect = [0 50 figWidth figHeight];
+figure('OuterPosition', rect)
+
+% real
+bnds = [1.5 5]; % freq bnds
+ind = dof.vert.super;
+plot(f,real(pxy(:,ind)))
+title(['CPSD - Vertical - Superstructure - Real - Reference: ' num2str(refInd)]);
+xlabel('Frequency [Hz]')
+ylabel('Power [dB]')
+xlim(bnds) 
+grid minor
+legend(dof.labels(ind))
+lh = get(gca,'children');   % find line handles of current axes
+set(lh,'linewidth',1);      % set linewidth 
+ah = gca;
+ah.FontName = 'Times';
+ah.FontSize = 18;
+snapnow
+
+
+% imaginary
+bnds = [1.5 5]; % freq bnds
+ind = dof.vert.super;
+plot(f,imag(pxy(:,ind)))
+title('CPSD - Vertical - Superstructure - Imaginary');
+xlabel('Frequency [Hz]')
+ylabel('Power [dB]')
+xlim(bnds) 
+grid minor
+legend(dof.labels(ind))
+lh = get(gca,'children');   % find line handles of current axes
+set(lh,'linewidth',1);      % set linewidth 
+ah = gca;
+ah.FontName = 'Times';
+ah.FontSize = 18;
+snapnow
+
+
+%% Pseudo Impulse Response Function (PIRF) and pH
+%
+%
+%
+
+ind = dof.vert.super;
+[pH,ff,pirf,tt] = getpH(data(:,ind),ind,fs,[]);
+
+
+
+%% Mode Shape Plots - Vertical DOF - Superstructure ONLY
+%
+%
+
+% get frequency index 
+peakFreqs = [2.106 2.148 2.435 2.533 3.204 3.345 3.448 3.503 3.558];
+[~,peakInd] = searchVector(f,peakFreqs);
+
+% create figure w/ increased resolution (from default)
+figWidth = 1120; % pixels
+figHeight = 840;
+rect = [0 50 figWidth figHeight];
+figure('OuterPosition', rect)
+
+
+mode = 2;
+scale = 25;
+xres = 50;
+yres = 25;
+
+U = normalizeMode(real(pxy(peakInd(mode),dof.vert.super))')
+ah = axes;
+plot_interpmode(ah,dof.coords(dof.vert.super,:),dof.bcoords,U,xres,yres,scale);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ code/plot_interpmode.m
@@ -0,0 +1,71 @@
+function plot_interpmode(ah,coords,bcoords,z,xres,yres,sc)
+%% SYNTAX: function plotInterpMode(ah,x,y,z,xres,yres,scale)
+%
+% jdv 2/15/14; 09222015
+
+    % error screen null bcoords
+    if isempty(bcoords)
+        % just use coords
+        xx = coords(:,1);
+        yy = coords(:,2); 
+        zz = z;
+    else
+        % concat boundaries
+        xx = [coords(:,1); bcoords(:,1)];
+        yy = [coords(:,2); bcoords(:,2)];
+        zz = [z; bcoords(:,3)];  
+    end
+    
+    % interpolate
+    xv = linspace(min(xx), max(xx),xres);
+    yv = linspace(min(yy), max(yy),yres);
+    [xInterp,yInterp] = meshgrid(xv,yv);
+    zInterp = griddata(xx,yy,zz,xInterp,yInterp,'v4');
+    
+    % plot shape to axes
+%     mesh(ah,xInterp,yInterp,zInterp*sc);
+    surf(ah,xInterp,yInterp,zInterp*sc);
+    hold(ah,'on')
+    % overlay DOF in red
+    plot3(ah,coords(:,1),coords(:,2),z*sc,'marker','o',...              
+                                          'markerfacecolor','k',...
+                                          'markeredgecolor','k',...
+                                          'linestyle','none',...
+                                          'markersize',6)    
+    % overlay boundaries in black
+    if ~isempty(bcoords)     % error screen null entry
+        plot3(ah,bcoords(:,1),bcoords(:,2),bcoords(:,3),...
+            'marker','.','color','k','linestyle','none');
+    end
+    hold(ah,'off');
+    axis(ah,'equal');
+    
+    % set zlim 
+    thresh = 1.2; % extend zlimits by 20% 
+    zlim(ah,[-1*max(abs(z*sc*thresh)) max(abs(z*sc*thresh))]);
+    
+    % show normalized modal amplitude on z-axis
+    tk = [-1 -.75 -.5 -.25 0 .25 .5 .75 1];
+    set(ah,'ztick',max(abs(zz))*abs(sc)*tk,'zticklabel',tk);
+    
+    % additional formatting
+    alpha(.9); % transparency value
+    grid(ah,'minor');
+    set(ah,'fontname','times new roman','fontsize',16);
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
